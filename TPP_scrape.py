24c8ac1af12f142f6b3362765926d247912216f1
jontonsoup4 <jontonsoup4@gmail.com>
2015-11-21 14:37:21 -0600

    cleaned up TPP_scrape.py

--- TPP_scrape.py
+++ TPP_scrape.py
@@ -1,6 +1,7 @@
-from bs4 import BeautifulSoup
 import requests
 import json
+
+from bs4 import BeautifulSoup
 from collections import OrderedDict
 
 
@@ -11,15 +12,21 @@ class TPP:
             self.index = json.loads(open('TPP.json').read(), object_pairs_hook=OrderedDict)
 
         except FileNotFoundError:
-            self.gather_data()
+            self.write_to_json()
 
-    def gather_data(self):
-        self.load_index()
-        for link in self.index.keys():
-            self.index[link]['text'] = self.search(self.index[link]['url'])
-            print(link)
-        with open('TPP.json', 'w') as f:
-            json.dump(self.index, f)
+    def get_text(self, url):
+        url = requests.get(url)
+        soup = BeautifulSoup(url.text, 'html.parser')
+        body = soup.find('div', {'class': 'section-content'})
+        text = []
+        for paragraph in body:
+            for line in paragraph:
+                if 'Read Previous Chapter' in line.text:
+                    break
+                if line.text == '':
+                    continue
+                text.append(line.text)
+        return text
 
     def load_index(self):
         url = requests.get(self.url)
@@ -34,29 +41,23 @@ class TPP:
                 index['Chapter {}'.format(ch_num)] = {'title': chapter.find('a').text, 'url': chapter.find('a')['href']}
             ch_num += 1
 
-    def search(self, url):
-        url = requests.get(url)
-        soup = BeautifulSoup(url.text, 'html.parser')
-        body = soup.find('div', {'class': 'section-content'})
-        text = []
-        for paragraph in body:
-            for line in paragraph:
-                if 'Read Previous Chapter' in line.text:
-                    break
-                if line.text == '':
-                    continue
-                text.append(line.text)
-        return text
+    def write_to_json(self):
+        self.load_index()
+        for link in self.index.keys():
+            self.index[link]['text'] = self.get_text(self.index[link]['url'])
+            print(link)
+        with open('TPP.json', 'w') as f:
+            json.dump(self.index, f)
 
     def write_to_text(self):
         with open('TPP.txt', 'w') as f:
             for chapter in self.index:
                 print(chapter)
-                cur = self.index[chapter]
+                current_chapter = self.index[chapter]
                 f.write('*************** {} ***************'.format(chapter) + '\n\n')
-                f.write('--- {} ---'.format(cur['title']) + '\n\n')
-                f.write('url: ({})'.format(cur['url']) + '\n\n')
-                for line in cur['text']:
+                f.write('--- {} ---'.format(current_chapter['title']) + '\n\n')
+                f.write('url: ({})'.format(current_chapter['url']) + '\n\n')
+                for line in current_chapter['text']:
                     f.write(line + '\n\n')
                 f.write('\n')
             f.write('------------------------------' + '\n')
